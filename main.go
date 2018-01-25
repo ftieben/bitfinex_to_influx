590d57b1b94fdbd3176dd0c8ea33d874fda8ddee
ftieben <ftiebe@gmail.com>
2018-01-25 01:05:37 +0100

    much more elegant

--- main.go
+++ main.go
@@ -5,34 +5,42 @@ import (
 	"log"
 	"math/rand"
 	"os"
-	"strconv"
+	"strings"
 	"time"
 
 	"github.com/bitfinexcom/bitfinex-api-go/v1"
 	"github.com/influxdata/influxdb/client/v2"
 )
 
-func writePoints(tags map[string]string, fields map[string]interface{}) {
-	fmt.Println(tags)
+func writePoints(fields map[string]interface{}) {
+
+	// Conntect to Influx
+	InfluxHost := os.Getenv("INFLUX_HOST")
+	InfluxDB := os.Getenv("INFLUX_DB")
+	InfluxUser := os.Getenv("INFLUX_USER")
+	InfluxPass := os.Getenv("INFLUX_Pass")
+
 	clnt, err := client.NewHTTPClient(client.HTTPConfig{
-		Addr:     "http://127.0.0.1:8086",
-		Username: "crypto",
-		Password: "crypto",
+		Addr:     InfluxHost,
+		Username: InfluxUser,
+		Password: InfluxPass,
 	})
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err) //log.Fatal(err)
+
 	}
 
 	bp, err := client.NewBatchPoints(client.BatchPointsConfig{
-		Database:  "crypto",
+		Database:  InfluxDB,
 		Precision: "us",
 	})
 
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err) //log.Fatal(err)
 	}
 
 	rand.Seed(time.Now().UnixNano())
+	tags := make(map[string]string)
 
 	pt, err := client.NewPoint(
 		"crypto",
@@ -41,92 +49,54 @@ func writePoints(tags map[string]string, fields map[string]interface{}) {
 		time.Now(),
 	)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err) //log.Fatal(err)
 	}
 	bp.AddPoint(pt)
 
 	if err := clnt.Write(bp); err != nil {
+		fmt.Println(err) //log.Fatal(err)
+	}
+}
+
+func getData(element bitfinex.WalletBalance, client *bitfinex.Client) map[string]interface{} {
+	change := element.Currency + "usd"
+	Ticker, err := client.Ticker.Get(change)
+
+	if err != nil {
 		log.Fatal(err)
-	}
-}
+		return make(map[string]interface{})
 
-func getValue(curency string, client *bitfinex.Client) float64 {
-	change := curency + "usd"
-	var price float64
-	ticker, err := client.Ticker.Get(change)
-
-	if err != nil {
-		fmt.Println("Error")
 	} else {
-		price, err2 := strconv.ParseFloat(ticker.LastPrice, 64)
-		if err2 != nil {
-			fmt.Println(err2)
-		}
-		return price
+		returnMap := make(map[string]interface{})
+		returnMap["Curency"] = strings.ToUpper(element.Currency)
+		returnMap["Volume"] = Ticker.Volume
+		returnMap["Ask"] = Ticker.Ask
+		returnMap["Bid"] = Ticker.Bid
+		returnMap["High"] = Ticker.High
+		returnMap["LastPrice"] = Ticker.LastPrice
+		returnMap["Low"] = Ticker.Low
+		returnMap["Mid"] = Ticker.Mid
+		returnMap["Balance"] = element.Amount
+		return returnMap
 	}
-	return price
-}
-
-func getVolume(curency string, client *bitfinex.Client) float64 {
-
-	change := curency + "usd"
-	var price float64
-	ticker, err := client.Ticker.Get(change)
-
-	if err != nil {
-		fmt.Println("Error")
-	} else {
-
-		price, err2 := strconv.ParseFloat(ticker.Volume, 64)
-		if err2 != nil {
-			fmt.Println(err2)
-		}
-		return price
-
-	}
-	return price
 }
 
 func main() {
 	key := os.Getenv("BFX_API_KEY")
 	secret := os.Getenv("BFX_API_SECRET")
-	
+
 	client := bitfinex.NewClient().Auth(key, secret)
 
 	balance, err := client.Balances.All()
 
-	tags := map[string]string{
-		"kind": "value",
-	}
-
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 	} else {
 
 		for _, element := range balance {
+			fmt.Println(getData(element, client))
 
-			amountMap := make(map[string]interface{})
-			valueMap := make(map[string]interface{})
-			volumeMap := make(map[string]interface{})
-
-			valueMap[element.Currency] = getValue(element.Currency)
-			amountMap[element.Currency], err = strconv.ParseFloat(element.Amount, 64)
-			volumeMap[element.Currency] = getVolume(element.Currency, client)
-			if err != nil {
-				fmt.Println(err)
-			}
-			fmt.Println(element.Currency, valueMap[element.Currency], amountMap[element.Currency], volumeMap[element.Currency])
-
-			continue // Disable Influx Export
-			tags["kind"] = "value"
-			writePoints(tags, valueMap)
-
-			tags["kind"] = "amount"
-			writePoints(tags, amountMap)
-
-			tags["kind"] = "volume"
-			writePoints(tags, volumeMap)
-
+			writePoints(getData(element, client))
 		}
 	}
 }
